fe1e8b81a4108355f27d8597cfe7801a601e7ac1
Christian Go <cvgo2000@yahoo.com>
2020-12-05 11:33:18 -0700

    server.js updated

--- server.js
+++ server.js
@@ -16,4 +16,23 @@ app.use(express.json());
 
 
 //ROUTES
+//route to home page
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
+
+//route to table API
+app.get('/api/tables', (req, res) => res.json(tables));
+
+//route to wait list API
+app.get('/api/waitlist' (req, res) => res.json(waitlist)); 
+
+
+//Creating new reservations
+app.post('/api/tables', (req, res) => {
+    const newReservation = req.body;
+    console.log(newReservation);
+    res.json(newReservation);
+})
+
+
+// Listener
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
